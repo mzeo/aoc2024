28bd484ac8bb2da31c52ca2651af45f3d4be5fe9
Marcus Vesterlund <marcus.vesterlund@gmail.com>
2024-12-21 12:55:13 +0100

    Update day21.py

--- day21.py
+++ day21.py
@@ -12,10 +12,7 @@ arrows="""\
 <v>
 """
 
-hoola=[]
-
-@cache
-def shortest_path(keypad, a, b, level):
+def min_path(keypad, a, b, level):
     stride = keypad.find("\n")+1
     keya = keypad.find(a)
     keyb = keypad.find(b)
@@ -24,24 +21,24 @@ def shortest_path(keypad, a, b, level):
 
     result = []
     if keypad[keyb-dx]!=" ":
-        result += [expand("^v"[dy>0]*abs(dy)+"<>"[dx>0]*abs(dx)+"A",level-1)]
+        result += [sequence_length("^v"[dy>0]*abs(dy)+"<>"[dx>0]*abs(dx)+"A",level-1)]
     if keypad[keya+dx]!=" ":
-        result += [expand("<>"[dx>0]*abs(dx)+"^v"[dy>0]*abs(dy)+"A",level-1)]
+        result += [sequence_length("<>"[dx>0]*abs(dx)+"^v"[dy>0]*abs(dy)+"A",level-1)]
 
     return min(result)
 
 @cache
-def expand(keys, level, keypad=arrows):
+def sequence_length(keys, level, keypad=arrows):
     if level<0:
         return len(keys)
     result = 0
     for a,b in zip("A"+keys, keys):
-        result += shortest_path(keypad,a,b,level)
+        result += min_path(keypad,a,b,level)
     return result
 
 p2 = p1 = 0
 for code in open(0):
     numeric = int(code[:-2])
-    p1 += numeric * expand(code[:-1],2,numpad)
-    p2 += numeric * expand(code[:-1],25,numpad)
+    p1 += numeric * sequence_length(code[:-1],2,numpad)
+    p2 += numeric * sequence_length(code[:-1],25,numpad)
 print(p1, p2)
